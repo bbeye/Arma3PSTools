b8701f837bc08aff9f0b73c1d06a8fbd7947ba4e
Randy <brenman72@gmail.com>
2022-07-20 23:12:37 -0500

    updates to training mission consumer and ensuring start arma server works with specified modlist

---
+++ V1_Training_Server_Update/Training_Mission_Consumer_Watcher.ps1
@@ -0,0 +1,30 @@
+﻿<#
+https://stackoverflow.com/questions/37477187/run-ps-commands-every-time-a-file-is-added-to-a-folder
+
+ Get-EventSubscriber -SourceIdentifier FileCreated
+
+ Unregister-Event -SourceIdentifier FileCreated
+
+#>
+
+
+$folder = 'D:\Arma3\missions\17th_Training'
+$filter = '*.pbo'
+$fsw = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ IncludeSubdirectories = $true}
+$fsw.EnableRaisingEvents = $true
+
+$onCreated = Register-ObjectEvent $fsw Created -SourceIdentifier FileCreated -Action {
+    try {
+    $path = $Event.SourceEventArgs.FullPath
+    # A file has been added, run your ps commands here. e. g.
+    Start-Sleep -Seconds 5
+    D:\Arma3\scripts\Arma3PSTools\V1_Training_Server_Update\Update_Training_Map.ps1
+    Write-Host "Package Consumed at $(Get-Date)"
+    }
+    catch {
+    Write-Host "Error encountered, Watcher Reset"
+    }
+}
+
+
+
---
+++ V1_Training_Server_Update/Update_Training_Map.ps1
@@ -0,0 +1,15 @@
+﻿$file = "$($env:ARMAPATH)configs\17th_training\17th_training_server.cfg"
+
+$newestTrainingMap = Get-ChildItem D:\arma3\missions\17th_training | sort LastWriteTime | select -last 1
+$newestTrainingMap.basename
+
+Stop-Arma3Server -launchID 17th_training
+
+#template = "17TH_BN_Training_Facility_V1_3.isladuala3";
+
+$regex = 'template = ".*"'
+$replace = "template = `"$($newestTrainingMap.BaseName)`""
+
+(Get-Content $file) -replace $regex, $replace | Set-Content $file
+
+Start-Arma3Server -launchID 17th_training -Port 2402 -modlistSelect Automatic -runType Server
